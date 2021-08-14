07ce0ff2d1d0d2907baec3b3a9e445318c3715ea
Tushar Nayan <tusharnayan2610@gmail.com>
2021-08-14 15:26:40 +0530

    first commit

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+##Name: Tushar Nayan
+##URL: file:///F:/MY_CLIENTS/NGO/index.html
+##Author: Tushar Nayan
+##License: NULL
---
+++ app.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/portfolio-details", (req, res) => {
+    res.render("portfolio-details");
+});
+
+app.get("/inner-page", (req, res) => {
+    res.render("inner-page");
+});
+
+const PORT = 3000 || process.env.PORT
+app.listen(PORT, function () {
+    console.log("Server running on port " + PORT);
+});
